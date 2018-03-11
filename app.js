2fff01938bbf8248bd84d411146830fa3d23a564
Joon Young Lee <joonyoung82@gmail.com>
2018-03-11 17:03:18 -0400

    changed port

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
 
+var port = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
 
 app.get('/hello', (req, res) => res.send('hello calvin'))
 
-app.listen(80, () => console.log('Example app listening on port 3000!'))
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
